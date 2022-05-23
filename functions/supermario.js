8eef8f22df4245c147bd3d7ad86641f061532d08
Julie Yudowitch <julieyudo@gmail.com>
2022-05-23 07:41:40 -0400

    netlify function

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,7 +1,7 @@
 exports.handler = async () => {
     const data = { name: 'mario', age: 35, job: 'plumber' }
-    return (
+    return {
         statusCode: 200,
         body: JSON.stringify(data)
-    )
+}
 }
